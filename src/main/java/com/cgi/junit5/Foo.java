077269a1a694d9b809e7b837b58756e4932dbf61
ruby <hs.ruisun@gmail.com>
2018-07-26 11:42:30 -0300

    add junit5 package

---
+++ src/main/java/com/cgi/junit5/Foo.java
@@ -0,0 +1,8 @@
+package com.cgi.junit5;
+
+public class Foo {
+    String name;
+    public Foo(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/cgi/junit5/FooTest.java
@@ -0,0 +1,12 @@
+package com.cgi.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(FooParameterResolver.class)
+public class FooTest {
+    @Test
+    public void testIt(Foo fooInstance) {
+        System.out.println(fooInstance.name);
+    }  
+}
